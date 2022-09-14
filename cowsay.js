1bf36105087ab40ebac9b2cbe194bcb709a1d6b7
gramer12 <yid05011@naver.com>
2022-09-14 17:20:58 +0900

    init

---
+++ cowsay.js
@@ -0,0 +1,11 @@
+var cowsay = require("cowsay");
+var moment = require("moment");
+
+console.log(moment);
+console.log(
+  cowsay.say({
+    text: "돼지는 꿀꿀digimon112",
+    e: "oO",
+    T: "U ",
+  })
+);
---
+++ lodash.js
@@ -0,0 +1,6 @@
+var _ = require("lodash");
+
+const arr = [5, 4, 3, 2, 1];
+const reversArr = _.reverse(arr);
+
+console.log(reversArr);
